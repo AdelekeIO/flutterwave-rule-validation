83919b015cd33a23f7ac869afee43138f7404250
aio <tom.shegzy@gmail.com>
2021-01-28 17:26:41 +0100

    Main.js: Validations in progress

--- routes/api/main.js
+++ routes/api/main.js
@@ -1,11 +1,63 @@
 const express = require("express");
+const config = require("config");
 const router = express.Router();
+var _ = require("lodash");
+const me = config.get("myData");
+let struct = require("../../lib/constants.json");
 
-// @route   GET api/Product
-// @desc    Get All Products
+// @route   GET /
+// @desc    First route is the base route. HTTP GET "/"
 // @access  Public
 router.get("/", (req, res) => {
-  res.json("Welcome to sepermaket api v 0.1");
+  struct.response.data = me;
+  let response = struct.response;
+  res.json(response);
 });
+
+// validate-rule
+// @route   POST /
+// @desc     Second route is the rule validation route. HTTP POST "/validate-rule""
+// @access  Public
+router.post("/validate-rule", async (req, res) => {
+  let payload = req.body;
+  let fb = await validate(payload);
+  console.log({ fb });
+
+  if (fb == true && _.isBoolean(fb)) {
+    struct.response.data = me;
+    let response = struct.response;
+    res.json(response);
+  } else {
+    struct.errorResponse.message = fb;
+    let response = struct.errorResponse;
+    res.status(400).json(response);
+  }
+});
+
+const validate = (payload) => {
+  // Check if required field are supplied
+  let resp = hasFields(struct.inputRequiredFields, payload);
+  // console.log({ resp });
+
+  if (resp == true && _.isBoolean(resp)) {
+    // The rule field should be a valid JSON object and should contain the following required fields
+    let { rule } = payload;
+    let { rule: ruleData } = struct;
+    console.log({ rule, ruleData });
+    resp = _.isPlainObject(rule) ? true : ruleData.validJsonErrorMessage;
+
+    // _.isPlainObject(rule) &&
+    return resp;
+  } else return resp;
+};
+
+const hasFields = (fields, payload) => {
+  return fields.reduce((fb, v) => {
+    if (!_.hasIn(payload, v.field)) {
+      fb = v.message;
+      return fb;
+    } else return true;
+  });
+};
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const mongoose = require("mongoose");
 const path = require("path");
 const config = require("config");
 
@@ -8,28 +7,9 @@ const app = express();
 // Bodyparser Middleware
 app.use(express.json());
 
-// DB Config
-const db = config.get("mongoURI");
-
-// Connect to Mongo
-mongoose
-  .connect(db, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false
-  }) // Adding new mongo url parser
-  .then(() => console.log("MongoDB Connected..."))
-  .catch(err => console.log(err));
-
 // Use Routes
-app.use("/api", require("./routes/api/main"));
-
-app.use("/api/users", require("./routes/api/users"));
-app.use("/api/auth", require("./routes/api/auth"));
-app.use("/api/messages", require("./routes/api/messages"));
-app.use("/api/products", require("./routes/api/products"));
-app.use("/api/productCategory", require("./routes/api/productCategory"));
+app.use("/", require("./routes/api/main"));
+app.use("/validate-rule", require("./routes/api/main"));
 
 // Serve static assets if in production
 // if (process.env.NODE_ENV === "production") {
